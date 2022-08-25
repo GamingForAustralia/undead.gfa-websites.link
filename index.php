fa393a6c48ecf2af9dcb380a3793f830cdabc041
SimDifRandomHD <lachlan@lochstudios.com>
2022-08-25 21:44:15 +1000

    undead.gfa-websites.link

---
+++ index.php
@@ -0,0 +1,231 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <!-- Page Information
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta charset="utf-8">
+  <title>GFAUnDead Links</title>
+  <meta name="description" content="Find us online!">
+  <meta name="author" content="Seth Cottle">
+  
+  <!-- Mobile Specific Metas
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- FONT
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800&display=swap" rel="stylesheet">
+  
+  <!-- CSS
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/skeleton-dark.css">
+  <link rel="stylesheet" href="css/brands.css">
+
+  <!-- Favicon
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link rel="icon" type="image/png" href="https://cdn.gfa-websites.link/GFALinks/GFAUnDeadLogo.png">
+
+</head>
+<body>
+
+  <!-- Primary Page Layout
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <div class="container">
+    <div class="row">
+      <div class="column" style="margin-top: 10%">
+
+        <!-- Your Image Here -->
+        <img src="https://cdn.gfa-websites.link/GFALinks/GFAUnDeadLogo.png" srcset="https://cdn.gfa-websites.link/GFALinks/GFAUnDeadLogo.png 4x" alt="GFA Logo">
+
+        <!-- Title -->
+        <h1>GFAUnDead</h1>
+
+        <!-- Short Bio -->
+        <p></p>
+
+        <!-- Github -->
+        <a class="button button-github" href="https://github.com/GFAUnDead" target="_blank" rel="noopener"><img class="icon" src="https://cdn.gfa-websites.link/GFALinks/icons/github.svg" alt="GitHub Logo">GitHub</a>
+          <br>
+
+        <!-- Twitter -->
+        <a class="button button-twit" href="https://twitter.com/GFAUnDead" target="_blank" rel="noopener"><img class="icon" src="https://cdn.gfa-websites.link/GFALinks/icons/twitter.svg" alt="Twitter Logo">Twitter</a>
+          <br>
+
+        <!-- Instagram -->
+        <a class="button button-instagram" href="https://www.instagram.com/GFAUnDead/" target="_blank" rel="noopener"><img class="icon" src="https://cdn.gfa-websites.link/GFALinks/icons/instagram.svg" alt="Instagram Logo">Instagram</a>
+          <br>
+
+        <!-- Facebook -->
+        <a class="button button-faceb" href="https://www.facebook.com/GFAUnDead" target="_blank" rel="noopener"><img class="icon" src="https://cdn.gfa-websites.link/GFALinks/icons/facebook.svg" alt="Facebook Logo">Facebook</a>
+          <br>
+
+        <!-- Facebook Messenger 
+        <a class="button button-messenger" href="" target="_blank" rel="noopener"><img class="icon" src="https://cdn.gfa-websites.link/GFALinks/icons/messenger.svg" alt="Facebook Messenger Logo">Chat on Messenger</a>
+          <br>   
+        -->
+
+        <!-- LinkedIn 
+        <a class="button button-linked" href="" target="_blank" rel="noopener"><img class="icon" src="https://cdn.gfa-websites.link/GFALinks/icons/linkedin.svg" alt="LinkedIn Logo">LinkedIn</a>
+          <br>
+        -->
+
+        <!-- YouTube -->
+        <a class="button button-yt" href="https://www.youtube.com/channel/UCVFA_AvilED8K5lm43GSiaQ" target="_blank" rel="noopener"><img class="icon" src="https://cdn.gfa-websites.link/GFALinks/icons/youtube.svg" alt="YouTube Logo">YouTube</a>
+          <br> 
+
+        <!-- Discord -->
+        <a class="button button-discord" href="https://discord.com/invite/B2GqChHK8k" target="_blank" rel="noopener"><img class="icon" src="https://cdn.gfa-websites.link/GFALinks/icons/discord.svg" alt="Discord Logo">Discord</a>
+          <br>
+
+        <!-- Twitch -->
+        <a class="button button-twitch" href="https://www.twitch.tv/GFAUnDead" target="_blank" rel="noopener"><img class="icon" src="https://cdn.gfa-websites.link/GFALinks/icons/twitch.svg" alt="Twitch Logo">Twitch</a>
+          <br>
+
+        <!-- ProductHunt 
+        <a class="button button-producthunt" href="" target="_blank" rel="noopener"><img class="icon" src="https://cdn.gfa-websites.link/GFALinks/icons/producthunt.svg" alt="Product Hunt Logo">Product Hunt</a>
+          <br>
+        -->
+
+        <!-- Snapchat 
+        <a class="button button-snapchat" href="" target="_blank" rel="noopener"><img class="icon" src="https://cdn.gfa-websites.link/GFALinks/icons/snapchat.svg" alt="Snapchat Logo">Snapchat</a>
+          <br>
+        -->
+
+        <!-- Spotify -->
+        <a class="button button-spotify" href="https://open.spotify.com/user/undeadlokko?si=88be351f21fd4785" target="_blank" rel="noopener"><img class="icon" src="https://cdn.gfa-websites.link/GFALinks/icons/spotify.svg" alt="Spotify Logo">Spotify</a>
+          <br>      
+        
+        <!-- Reddit -->
+        <a class="button button-reddit" href="https://www.reddit.com/user/gfaundead" target="_blank" rel="noopener"><img class="icon" src="https://cdn.gfa-websites.link/GFALinks/icons/reddit.svg" alt="Reddit Logo">Reddit</a>
+          <br>   
+
+        <!-- Mastodon 
+        <a class="button button-mastodon" href="" target="_blank" rel="noopener"><img class="icon" src="https://cdn.gfa-websites.link/GFALinks/icons/mastodon.svg" alt="Mastodon Logo">Mastodon</a>
+          <br>           
+        -->
+
+        <!-- Medium 
+        <a class="button button-medium" href="" target="_blank" rel="noopener"><img class="icon" src="https://cdn.gfa-websites.link/GFALinks/icons/medium.svg" alt="Medium Logo">Medium</a>
+          <br> 
+        -->
+
+        <!-- Pinterest
+        <a class="button button-pinterest" href="" target="_blank" rel="noopener"><img class="icon" src="https://cdn.gfa-websites.link/GFALinks/icons/pinterest.svg" alt="Pinterest Logo">Follow on Pinterest</a>
+          <br>
+        -->
+
+        <!-- TikTok -->
+        <a class="button button-tiktok" href="https://www.tiktok.com/@GFAUnDead" target="_blank" rel="noopener"><img class="icon" src="https://cdn.gfa-websites.link/GFALinks/icons/tiktok.svg" alt="TikTok Logo">TikTok</a>
+          <br>
+
+        <!-- Email -->
+        <a class="button button-default" href="mailto:contact@gfaundead.stream" target="_blank" rel="noopener"><img class="icon" src="https://cdn.gfa-websites.link/GFALinks/icons/email.svg" alt="Email Icon">Email Contact</a>
+          <br>
+
+        <!-- Email Alternative 
+        <a class="button button-default" href="" target="_blank" rel="noopener"><img class="icon" src="https://cdn.gfa-websites.link/GFALinks/icons/email_alt.svg" alt="Email Icon">@</a>
+          <br>
+			  -->
+        
+        <!-- SoundCloud -->
+        <a class="button button-soundcloud" href="https://soundcloud.com/GFAUnDead" target="_blank" rel="noopener"><img class="icon" src="https://cdn.gfa-websites.link/GFALinks/icons/soundcloud.svg" alt="SoundCloud Logo">SoundCloud</a>
+          <br>              
+
+        <!-- Figma 
+        <a class="button button-figma" href="" target="_blank" rel="noopener" ><img class="icon" src="https://cdn.gfa-websites.link/GFALinks/icons/figma.svg" alt="Figma Logo">Figma</a>
+          <br>
+        -->
+
+        <!-- Kit 
+        <a class="button button-kit" href="" target="_blank" rel="noopener"><img class="icon" src="https://cdn.gfa-websites.link/GFALinks/icons/kit.svg" alt="Kit Logo">Kit</a>
+          <br>          
+        -->
+
+        <!-- Telegram
+        <a class="button button-telegram" href="" target="_blank" rel="noopener"><img class="icon" src="https://cdn.gfa-websites.link/GFALinks/icons/telegram.svg" alt="Telegram Logo">Telegram</a>
+          <br>          
+        -->
+
+        <!-- Tumblr 
+        <a class="button button-tumb" href="" target="_blank" rel="noopener"><img class="icon" src="https://cdn.gfa-websites.link/GFALinks/icons/tumblr.svg" alt="Tumblr Logo">Tumblr</a>
+          <br>
+		  	-->
+        
+        <!-- Steam -->
+        <a class="button button-steam" href="https://steamcommunity.com/groups/GamingForAustralia" target="_blank" rel="noopener"><img class="icon" src="https://cdn.gfa-websites.link/GFALinks/icons/steam.svg" alt="Steam Logo">Steam</a>
+          <br>
+
+        <!-- Vimeo 
+        <a class="button button-vimeo" href="" target="_blank" rel="noopener"><img class="icon" src="https://cdn.gfa-websites.link/GFALinks/icons/vimeo.svg" alt="Vimeo Logo">Vimeo</a>
+          <br>
+        -->
+
+        <!-- PayPal -->
+        <a class="button button-paypal" href="https://paypal.me/gfaundead" target="_blank" rel="noopener"><img class="icon" src="https://cdn.gfa-websites.link/GFALinks/icons/paypal.svg" alt="PayPal Logo">PayPal</a>
+          <br>
+
+        <!-- WhatsApp 
+        <a class="button button-whatsapp" href="" target="_blank" rel="noopener"><img class="icon" src="https://cdn.gfa-websites.link/GFALinks/icons/whatsapp.svg" alt="WhatsApp Logo">WhatsApp</a>
+          <br>
+        -->
+
+        <!-- Wordpress 
+        <a class="button button-wordpress" href="" target="_blank" rel="noopener"><img class="icon" src="https://cdn.gfa-websites.link/GFALinks/icons/wordpress.svg" alt="Wordpress Logo">Wordpress</a>
+          <br>
+        -->
+
+        <!-- Goodreads
+        <a class="button button-goodreads" href="" target="_blank" rel="noopener"><img class="icon" src="https://cdn.gfa-websites.link/GFALinks/icons/goodreads.svg" alt="Goodreads Logo">Goodreads</a>
+          <br>
+        -->
+
+        <!-- Skoob
+        <a class="button button-skoob" href="" target="_blank" rel="noopener"><img class="icon" src="https://cdn.gfa-websites.link/GFALinks/icons/skoob.svg" alt="Skoob Logo">Skoob</a>
+          <br>
+        -->
+
+        <!-- Gitlab 
+        <a class="button button-gitlab" href="" target="_blank" rel="noopener"><img class="icon" src="https://cdn.gfa-websites.link/GFALinks/icons/gitlab.svg" alt="GitLab Logo">GitLab</a>
+          <br>
+        -->
+
+        <!-- XING 
+        <a class="button button-xing" href="" target="_blank" rel="noopener"><img class="icon" src="https://cdn.gfa-websites.link/GFALinks/icons/xing.svg" alt="xing Logo">Xing</a>
+          <br>
+        -->
+
+        <!-- Bandcamp 
+        <a class="button button-bandcamp" href="" target="_blank" rel="noopener"><img class="icon" src="https://cdn.gfa-websites.link/GFALinks/icons/bandcamp.svg" alt="Bandcamp Logo">Bandcamp</a>
+          <br>
+        -->
+
+        <!-- Patreon -->
+        <a class="button button-patreon" href="https://www.patreon.com/GFAUnDead" target="_blank" rel="noopener"><img class="icon" src="https://cdn.gfa-websites.link/GFALinks/icons/patreon.svg" alt="Patreon Logo">Patreon</a>
+          <br>
+
+        <!-- Signal
+        <a class="button button-signal" href="" target="_blank" rel="noopener"><img class="icon" src="https://cdn.gfa-websites.link/GFALinks/icons/signal.svg" alt="Signal Logo">Signal</a>
+          <br>
+        -->
+
+        <!-- Venmo
+        <a class="button button-venmo" href="" target="_blank" rel="noopener"><img class="icon" src="https://cdn.gfa-websites.link/GFALinks/icons/venmo.svg" alt="Venmo Logo">Venmo</a>
+          <br>
+        -->
+
+        <br>
+        <br>
+        <p>&copy; 2016-<?php date_default_timezone_set("Australia/Sydney");?><?=date("Y");?>, GFAUnDead<br>
+          GFAUnDead is a subsidiary and is registered under LochsMedia, fully registered in Australia under Business Number: 20447022747
+        </p>
+          
+      </div>
+    </div>
+  </div>
+
+<!-- End Document
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+</body>
+</html>
